a9cbd342d3b4588fd0506c6ab81ecdd53d15af75
Paul Sonkoly <sonkoly.pal@gmail.com>
2019-01-03 11:29:22 +0000

    Added exception for missing language
    It failed to render anyways, but without a meaningful error message

--- lib/markdown_renderer.rb
+++ lib/markdown_renderer.rb
@@ -3,6 +3,7 @@ require 'coderay'
 
 class MarkdownRenderer < Redcarpet::Render::HTML
   def block_code(code, language)
+    raise "No language is given for code #{code}" if language.nil?
     CodeRay.highlight(code, language)
   end
 end
